24e29f49cd5ac37c723eef8fd14e82bd14671e10
Brandon Lublin <43397446+brandonlublin@users.noreply.github.com>
2025-02-14 06:14:40 -0800

    add CNAME back and rebuild

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -20,7 +20,6 @@ const ProjectCard = ({ project, onSelectProject }: ProjectCardProps) => {
     <div className={styles.projectCard} id={project?.id} onClick={() => onSelectProject(project)}>
       <div className={styles.projectContent}>
         <div className={styles.projectTopRow}>
-          <img className={styles.projectImg} src={project?.img} alt={project?.imgAlt} />
           <h3 className={styles.projectTitle}>{project?.title}</h3>
         </div>
         <p className={styles.description}>{project?.description}</p>
